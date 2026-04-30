17bb2954d8a7d7fe080e12314ee33fe683760cb5
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-04-29 21:52:33 -0700

    implemented search and filter functionality on Browse.jsx

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,116 +1,153 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Browse = () => {
+  const [searchId, setSearchId] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [searchId, setSearchId] = useState(null)
-    const [movies, setMovies] = useState([])
-    const [loading, setLoading] = useState(true)
+  async function fetchMovies(searchTerm = "fast") {
+    try {
+      setLoading(true);
 
-    async function fetchMovies() {
-        try {
-            const { data } = await axios.get(
-                `https://www.omdbapi.com/?&apikey=d051fbc2&s=fast`
-            )
-            console.log(data.Search)
-            setMovies(data.Search)
-            setLoading(false)
-        } catch {
-            alert("Unable to fetch movies")
-        }
+      const { data } = await axios.get(
+        `https://www.omdbapi.com/?&apikey=d051fbc2&s=${encodeURIComponent(
+          searchTerm
+        )}`
+      );
+
+      console.log(data.Search);
+
+      // Empty array prevents React from crashing if Search returns with "undefined"
+      setMovies(data.Search || []);
+    } catch {
+      alert("Unable to fetch movies");
+    } finally {
+      setLoading(false);
     }
+  }
 
-    async function searchMovies(searchId) {
-        const { data } = await axios.get(`https://www.omdbapi.com/?&apikey=d051fbc2&s=${searchId}`)
+//   Function for search queries - .trim is to prevent users from searching with empty spaces. Reuses fetch movies.
+  function onSearch() {
+    if (!searchId.trim()) return;
+    fetchMovies(searchId);
+  }
+
+
+//   Function for filter button
+  function filterMovies(event) {
+    const filter = event.target.value;
+    const sorted = [...movies];
+
+    if (filter === "A_TO_Z") {
+      sorted.sort((a, b) => a.Title.localeCompare(b.Title));
+    } else if (filter === "Z_TO_A") {
+      sorted.sort((a, b) => b.Title.localeCompare(a.Title));
+    } else if (filter === "NEWEST_TO_OLDEST") {
+      sorted.sort((a, b) => b.Year - a.Year);
+    } else if (filter === "OLDEST_TO_NEWEST") {
+      sorted.sort((a, b) => a.Year - b.Year);
     }
+    setMovies(sorted);
+  }
 
-    function onSearch() {
-        fetchMovies(searchId);
-    }
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
-    useEffect(() => {
-        fetchMovies()
-    }, [])
-
-    return (
-        <>
-            <div className="browse__bar">
-                <div className="browse__bar--wrapper">
-                    <h1 className="h1__browse">Browse Movies & TV Shows</h1>
-                    <div className="input__wrapper">
-                        <input
-                            type="search"
-                            value={searchId}
-                            placeholder="Search for a movie or TV show..."
-                            className="browse__input"
-                            id="searchInput"
-                            onChange={(event) => setSearchId(event.target.value)}
-                            onKeyDown={(event) => {if(event.key === 'Enter') onSearch()}}
-                        />
-                        <div className="search__wrapper" id="searchBtn" onClick={() => onSearch()}>
-                            <FontAwesomeIcon
-                                icon={faMagnifyingGlass}
-                                className="fa-solid fa-magnifying-glass search__icon"
-                            />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="browse__bar">
+        <div className="browse__bar--wrapper">
+          <h1 className="h1__browse">Browse Movies & TV Shows</h1>
+          <div className="input__wrapper">
+            <input
+              type="search"
+              value={searchId}
+              placeholder="Search for a movie or TV show..."
+              className="browse__input"
+              id="searchInput"
+              onChange={(event) => setSearchId(event.target.value)}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") onSearch();
+              }}
+            />
+            <div
+              className="search__wrapper"
+              id="searchBtn"
+              onClick={() => onSearch()}
+            >
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className="fa-solid fa-magnifying-glass search__icon"
+              />
             </div>
-            <div className="overlay"> </div>
-            <div className="progress__bar">
-                <div className="progress">
-                    <div className="indicator"></div>
-                </div>
-            </div>
-            {/* Search results section */}
-            <section id="search">
-                <div className="filter__content--wrapper">
-                    <div className="search__label">
-                        <p className="search__info">Search results:</p>
-                    </div>
-                    <div className="filter">
-                        <select id="filter" onChange="filterMovies(event)">
-                            <option value="disabledselected">Sort</option>
-                            <option value="A_TO_Z">A to Z</option>
-                            <option value="Z_TO_A">Z to A</option>
-                            <option value="NEWEST_TO_OLDEST">Newest to Oldest</option>
-                            <option value="OLDEST_TO_NEWEST">Oldest to Newest</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="content__wrapper">
-                    <div className="movies movies__loading" id="movie__img--wrapper">
-                        {loading ? (
-                            <div className="movies__skeleton">
-                                <div className="spinner__wrapper">
-                                    <FontAwesomeIcon icon={faSpinner} className="fa-solid fa-spinner movies__loading--spinner" />
-                                </div>
-                                {new Array(10).fill(0).map((_, index) => (
-                                    <div className="movie__skeleton" key={index}></div>
-                                ))}
-                            </div>
-                        ) : (
-                            movies.map((movie) => (
-                                <div className="movie" key={movie.imdbID}>
-                                    <figure className="movie__img--wrapper">
-                                        <img src={movie.Poster} alt="" className="movie__img" />
-                                    </figure>
-                                    <div className="movie__title">
-                                        <span className="movie__details">{movie.Title}</span>
-                                    </div>
-                                    <div className="movie__year">
-                                        <span>{movie.Year}</span>
-                                    </div>
-                                </div>
-                            ))
-                        )}
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
+      <div className="overlay"> </div>
+      <div className="progress__bar">
+        <div className="progress">
+          <div className="indicator"></div>
+        </div>
+      </div>
 
-export default Browse
+      {/* Search results section */}
+      <section id="search">
+        <div className="filter__content--wrapper">
+          <div className="search__label">
+            <p className="search__info">Search results:</p>
+          </div>
+          {/* Filter button */}
+          <div className="filter">
+            <select id="filter" onChange={(event) => filterMovies(event)}>
+              <option value="disabledselected">Sort</option>
+              <option value="A_TO_Z">A to Z</option>
+              <option value="Z_TO_A">Z to A</option>
+              <option value="NEWEST_TO_OLDEST">Newest to Oldest</option>
+              <option value="OLDEST_TO_NEWEST">Oldest to Newest</option>
+            </select>
+          </div>
+        </div>
+        <div className="content__wrapper">
+          <div className="movies movies__loading" id="movie__img--wrapper">
+            {loading ? (
+              <div className="movies__skeleton">
+                <div className="spinner__wrapper">
+                  <FontAwesomeIcon
+                    icon={faSpinner}
+                    className="fa-solid fa-spinner movies__loading--spinner"
+                  />
+                </div>
+                {new Array(10).fill(0).map((_, index) => (
+                  <div className="movie__skeleton" key={index}></div>
+                ))}
+              </div>
+            ) : (
+              movies.map((movie) => (
+                <div className="movie" key={movie.imdbID}>
+                  <figure className="movie__img--wrapper">
+                    <img src={movie.Poster} alt="" className="movie__img" />
+                  </figure>
+                  <div className="movie__title">
+                    <span className="movie__details">{movie.Title}</span>
+                  </div>
+                  <div className="movie__year">
+                    <span>{movie.Year}</span>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Browse;
